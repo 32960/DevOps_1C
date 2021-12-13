226484d60689cfb851182c826e4844e7030a918a
Administrator <admin@example.com>
2021-12-13 14:28:08 +0300

    allureRun

--- allureRun.cmd
+++ allureRun.cmd
@@ -1,5 +1,5 @@
 @chcp 65001
 
 # тестовые настройки:call allure generate --clean .\out\allure .\build\tests\allure -o .\build\allure-report && allure open .\build\allure-report
-call allure generate --clean .\out\allure .\out\smoke\allure .\out\syntax-check\allure -o .\out\allure\allure-report && allure open .\out\allure\allure-report
+call allure generate --clean .\out\allure\allure .\out\smoke\allure .\out\syntax-check\allure -o .\out\allure\allure-report && allure open .\out\allure\allure-report
 # Для PowerShell: allure generate --clean .\out\allure .\out\smoke\allure .\out\syntax-check\allure -o .\out\allure\allure-report; allure open .\out\allure\allure-report
